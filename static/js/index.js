4fbfda273359307c2d237fe94ebc219f33171744
Saaketh Bhojanam <136840409+SaakiBhojanam@users.noreply.github.com>
2024-05-31 22:11:48 -0500

    Formatted Everything with Prettier - Industry Standard

--- static/js/index.js
+++ static/js/index.js
@@ -6,15 +6,15 @@ let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 var sec = document.getElementById('sec')
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    moon.style.top = value * 0+'px';
-    mountains_behind.style.top = value *0.5 + 'px';
+    moon.style.top = value * 0 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
-    text.style.marginRight = value *1 +'px';
-    text.style.marginTop = value *1 +'px';
-    btn.style.marginTop = value *1.5 +'px';
-    sec.style.left = value *0.5+'px';
+    text.style.marginRight = value * 1 + 'px';
+    text.style.marginTop = value * 1 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    sec.style.left = value * 0.5 + 'px';
 })
 
 
